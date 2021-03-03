426a2f4ff6cdc4f5a3960aa1f64aac122e4d28e8
Kyle Pockett <60449328+Fuzzmaster@users.noreply.github.com>
2021-03-03 16:08:05 -0700

    Add files via upload

---
+++ cataco/assets/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var signup = document.getElementById("signup");
+
+    signup.addEventListener("click", function(evt){
+        javascript:showPopupSmart11420(true);
+        evt.preventDefault();
+    });
+}
